4e0e1617a99ff5d8d3596bc94cc471039ea33c55
Ronny Toribio <ronnytoribio1@hotmail.com>
2020-02-16 17:31:14 -0500

    initial commit

---
+++ Weather/Weather_api.py
@@ -0,0 +1,53 @@
+import json, requests
+
+
+api_key = '2c47a7c36e59270ba2c88bf07835fc6c'
+Base_url ='http://api.openweathermap.org/data/2.5/weather?'
+
+country_code = 'us'
+#TODO: create class so it'll be easir for geting information from the user 
+
+        
+
+def get_temp(zip_code):
+    '''this will get the current temp''' 
+
+    full_url = f'{Base_url}zip={zip_code},{country_code}&units=imperial&appid={api_key}'
+    x = requests.get(full_url).json()
+    return x['main']['temp']
+
+def get_description(zip_code):
+    '''this will get the description of the weather ''' 
+
+    full_url = f'{Base_url}zip={zip_code},{country_code}&units=imperial&appid={api_key}'
+    x = requests.get(full_url).json()
+    return x['weather'][0].get('description')
+
+def get_LocalName(zip_code):
+    '''This will get the name of the localtion ''' 
+
+    full_url = f'{Base_url}zip={zip_code},{country_code}&units=imperial&appid={api_key}'
+    x = requests.get(full_url).json()
+    return x['name']
+
+
+def get_icon(zip_code):
+    ''' This will get the icon '''
+    
+    full_url = f'{Base_url}zip={zip_code},{country_code}&units=imperial&appid={api_key}'
+    x = requests.get(full_url).json()
+    return x['weather'][0].get('icon')
+
+def check_zip(zip_code):
+    ''' cheks for valit Zip ''' 
+
+    full_url = f'{Base_url}zip={zip_code},{country_code}&units=imperial&appid={api_key}'
+    x = requests.get(full_url).json()
+    if x.get('cod') == 200:
+        return True
+    else:
+        raise SyntaxError
+
+
+conditions = {'group1':['clear sky','few clouds'],'group2':['scattered clouds','broken clouds','shower rain'],'group3':['rain','thunderstorm'],'group4':['snow','mist']}
+
---
+++ Weather/routes.py
@@ -0,0 +1,44 @@
+from Weather import app
+from Weather.Weather_api import get_temp, get_description, get_LocalName, get_icon, conditions
+from flask import render_template, send_from_directory, url_for, request, redirect
+
+
+
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        zipcode = request.form.get('zipcode')  
+        # TODO: ADD for validation !    
+        return  redirect(url_for('weather', zipcode=zipcode))
+    return render_template('home.html')
+
+
+@app.route('/weather')
+def weather():
+    zipcode = str(request.args.get('zipcode', None))
+    description = str(get_description(zipcode))
+    temp = str(round(get_temp(zipcode)))+'°'
+    location = str(get_LocalName(zipcode))
+    Weather_img = f'http://openweathermap.org/img/wn/{get_icon(zipcode)}@2x.png'
+    if description in conditions['group1'] and temp >= '50':
+        playList_Song = url_for('static', filename='playlists/Roddy_Ricch-The_Box.mp3')
+        print(f'{description} and {temp}')
+    elif description in conditions['group2'] and temp >= '35':
+        playList_Song = url_for('static', filename='playlists/Ray_Charles-I_got_a_woman.mp3')
+        print(f'{description} and {temp}')
+    elif description in conditions['group3'] and temp >= '40':
+        playList_Song = url_for('static', filename='playlists/Suicidal_Thoughts(2005_Remaster).mp3')
+        print(f'{description} and {temp}')
+    elif description in conditions['group4'] and temp <= '20':
+        playList_Song = url_for('static', filename='playlists/Syl_Johnson-Is_It_Because_Im_Black_Single.mp3')
+        print(f'{description} and {temp}')
+    else:
+        playList_Song = url_for('static', filename='playlists/James_Brown-Blues_and_Pants.mp3')
+        print(f'{description} and {temp}')
+    return render_template('weather.html', Weather_img=Weather_img, Song=playList_Song, temp=temp, location=location)
+
+
+@app.route('/test')
+def test():
+    return render_template('layout.html')
